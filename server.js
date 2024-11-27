102261c885be1d664e605bece39ccc75c2d92368
Caio Magno <caiormagno@gmail.com>
2024-11-26 21:51:34 -0300

    Novas configurações

--- server.js
+++ server.js
@@ -1,57 +1,10 @@
 import express from "express";
-
-
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 2,
-        descricao: "Gato brincando",
-        imagem: "https://placecats.com/whiskers/400/200",
-    },
-    {
-        id: 3,
-        descricao: "Gato dormindo",
-        imagem: "https://placecats.com/snuggle/500/250",
-    },
-    {
-        id: 4,
-        descricao: "Gato curioso",
-        imagem: "https://placecats.com/curious/300/300",
-    },
-    {
-        id: 5,
-        descricao: "Gato aventureiro",
-        imagem: "https://placecats.com/adventure/450/250",
-    },
-    {
-        id: 6,
-        descricao: "Gato elegante",
-        imagem: "https://placecats.com/fancy/350/200",
-    }
-];
-
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+
+routes(app);
 
 app.listen(3000, () => {
-    console.log("Servidor recebendo...");
-});
-
-app.get("/post", (req, res) => {
-    res.status(200).json(posts);
-});
-function buscarPostPorId(id){
-    return posts.findIndex((post)=>{
-        return post.id === Number(id)
-    })
-}
-
-app.get("/post/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id)
-    res.status(200).json(posts[index]);
+    console.log("Servidor escutando...");
 });
